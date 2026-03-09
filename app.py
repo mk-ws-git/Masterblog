9cf57a9f0be451760a509022f82a0d795e90c93e
mk-ws-git <maryksprague@gmail.com>
2026-03-09 11:33:18 +0100

    Create index route and define function to load blog posts

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
-
+def load_posts():
+    """Load blog posts from the JSON file."""
+    with open('blog_posts.json', 'r') as post:
+        return json.load(post)
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    posts = load_posts()
+    return render_template("index.html", posts=posts)
 
 
 if __name__ == '__main__':
